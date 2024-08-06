5634f532f2ff8ad472d1ea68a107bff3834c24e3
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-08-06 22:02:27 +0300

    add documentation

--- hello/hello.go
+++ hello/hello.go
@@ -13,6 +13,9 @@ func HelloWorld () {
 }
 
 func main() {
+	// Had to run go mod edit -replace learninggo/greetings=../greetings 
+	// to point go to the greetings module
+	// then => go mod tidy to resolve dependencies or build the tree (not sure yet)
 	HelloWorld()
 	message := greetings.Hello("Leo")
 	fmt.Println(message)

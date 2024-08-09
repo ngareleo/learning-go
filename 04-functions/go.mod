0b8488c40b3872f59e98150b91790ba11d03a912
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-08-09 20:47:01 +0300

    start functions

---
+++ 04-functions/go.mod
@@ -0,0 +1,3 @@
+module learninggo/functions
+
+go 1.22.2
---
+++ 04-functions/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("What's 10/42?", div(10, 42))
+}
+
+func div(num float32, denom float32) float32 {
+	if denom == 0 {
+		return 0
+	}
+	return float32(float32(num) / float32(denom))
+}

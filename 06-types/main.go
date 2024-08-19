58d3df5b83a14f976561b2fb188545d3aaec8bcb
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-08-19 18:50:51 +0300

    formt

--- 06-types/main.go
+++ 06-types/main.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
+// types are executable documentation. This is a common pattern in Go
+// Use types to describe data and what we expect
+type Year int
+type Speed int
+
+// This is a bad example but imagine this was something else with more complex behavior
+// and edge cases, or could be computed using a complex formula
+func (s Speed) compare(s2 Speed) (equal bool) {
+	return s2 > s
+}
+
+// By declaring speed, we tie together the concept of speed for (topSpeed, speed, speedHistory) Vehicle
 type Vehicle struct {
-	model string
-	year int
-	topSpeed int
-	speed int
-	speedHistory []int
+	model        string
+	year         Year
+	topSpeed     Speed
+	speed        Speed
+	speedHistory []Speed
 }
 
 // Below is a method (not function because we've bound it to a type)
 // Go allows you to declare methods only at the package level
-// It also restricts method declaration within the same file as the type declaration 
+// It also restricts method declaration within the same file as the type declaration
 // Meaning you can only bind types that you own
 // (v Vehicle) is a receiver specification. This is the type that will be bound to a function
 func (v Vehicle) String() string {
@@ -22,17 +34,19 @@ func (v Vehicle) String() string {
 // You can also define using pointer type receivers
 // In this case though, it is recommended to have consistency to avoid confusion around your types
 // If you pass a value specification, use value specification all through and the vice-versa
-func (v *Vehicle) Accelerate(by int) {
-	v.speed += by
-	v.speedHistory = append(v.speedHistory, v.speed)
+func (v *Vehicle) Accelerate(by Speed) {
+	if !(v.speed + by).compare(v.topSpeed) {
+		v.speed += by
+		v.speedHistory = append(v.speedHistory, v.speed)
+	}
 }
 
 func (v *Vehicle) fakeHistory() {
-	var fake = []int { 50, 60, 73, 119, 120}
+	var fake = []Speed{50, 60, 73, 119, 120}
 
 	// Best pattern is to code for nil values
 	if v == nil {
-		v = &Vehicle { speedHistory: []int{} }
+		v = &Vehicle{speedHistory: []Speed{}}
 	}
 
 	v.speedHistory = append(v.speedHistory, fake...)
@@ -41,20 +55,19 @@ func (v *Vehicle) fakeHistory() {
 func readFakeSpeedHistory(v Vehicle) {
 	// we call a pointer receiver method on a value function argument
 	v.fakeHistory()
-	for i, v := range(v.speedHistory) {
-		fmt.Printf("[record %d] %dkm/h\n", i + 1, v)
+	for i, v := range v.speedHistory {
+		fmt.Printf("[record %d] %dkm/h\n", i+1, v)
 	}
 }
 
-
-func main () {
-	v := Vehicle {
-		model: "Golf",
-		year: 2017,
+func main() {
+	v := Vehicle{
+		model:    "Golf",
+		year:     2017,
 		topSpeed: 220,
-		speed: 0,
+		speed:    0,
 	}
-	fmt.Println(v) 
+	fmt.Println(v)
 	// or
 	fmt.Println(v.String()) // less idiomatic
 
@@ -63,10 +76,11 @@ func main () {
 	v.Accelerate(10)
 	fmt.Println("Moving at", v.speed)
 
-	////////////////// WARNING ///////////////////////////////////////////
+	//////////////////////// [WARNING] ///////////////////////////////////
+	//                                                                  //
 	// If you call a value receiver method on a nil pointer instance,   //
 	// the code will compile but will panic during runtime              //
-	////////////////// WARNING ///////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
 
 	// Also function calls apply as usual
 	// If you call a pointer receiver method for a value parameter in a function, you mutate the copy
@@ -77,8 +91,9 @@ func main () {
 	readFakeSpeedHistory(v)
 	fmt.Println("Has the history changed?", v.speedHistory) // nope
 
-	var polo Vehicle;
+	var polo Vehicle
 	// Go allow you to call methods on nil instances of a certain type
 	polo.fakeHistory()
 	fmt.Println("Polo history", polo.speedHistory)
-}	
+
+}

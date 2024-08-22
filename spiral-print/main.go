badacd788299c6662bc260c31e580a78dcf26a69
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-08-22 11:16:45 +0300

    setup

--- spiral-print/main.go
+++ spiral-print/main.go
@@ -1,24 +1,48 @@
 package main
 
+import "fmt"
 
 // Prints an array in spiral
-func spiralPrint(spiral [][]int ) (out []int){
+func spiralPrint(spiral [][]int) (out []int) {
 	// print top edge
 	// do it in one loop
-	if len(spiral) < 1 {
+
+	if x := len(spiral); x < 1 {
+		return out
+	} else if y := len(spiral[0]); y < 1 {
+		return out
+	} else if x != y {
 		return out
 	}
-	
-	out = make([]int, len(spiral) * len(spiral[0]))
 
-	
+	sizeY := len(spiral)
+	sizeX := len(spiral[0])
 
-	for i, row := range spiral {
-		for j, val := range row {
+	out = make([]int, sizeX*sizeY)
 
+	// plan is to map (x, y) to an index to the final array
+	for x, row := range spiral {
+		for y, val := range row {
+			pos := mapXYToCoordinate(x, y, sizeX, sizeY)
+			out[pos] = val
 		}
 	}
 
+	return
 }
 
-func main () {}
+func mapXYToCoordinate(x, y, sizeX, sizeY int) int {
+	return 0
+}
+
+func main() {
+	arr := [][]int{
+		{0, 1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10, 11},
+		{12, 13, 14, 15, 16, 17},
+		{18, 19, 20, 21, 22, 23},
+	}
+	spiral := spiralPrint(arr)
+	fmt.Println("Printing", arr, "in a spiral")
+	fmt.Println(spiral)
+}
